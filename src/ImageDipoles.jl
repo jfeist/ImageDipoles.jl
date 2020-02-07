cd78540f38dba7593f0458223c011429312ea650
Johannes Feist <johannes.feist@gmail.com>
2020-02-07 09:34:51 +0100

    several updates

--- src/ImageDipoles.jl
+++ src/ImageDipoles.jl
@@ -72,7 +72,16 @@ Efun(cd::ChargesDipoles) = r -> sum(E_chargedipole.(cd.rs,cd.qs,cd.ds,(r,)));
 ϕfun(cd1::ChargesDipoles,cd2::ChargesDipoles) = r -> sum(ϕ_chargedipole.(cd1.rs,cd1.qs,cd1.ds,(r,))) + sum(ϕ_chargedipole.(cd2.rs,cd2.qs,cd2.ds,(r,)));
 Efun(cd1::ChargesDipoles,cd2::ChargesDipoles) = r -> sum(E_chargedipole.(cd1.rs,cd1.qs,cd1.ds,(r,))) + sum(E_chargedipole.(cd2.rs,cd2.qs,cd2.ds,(r,)));
 
-function sphere_chargedipole_images(r,q,d,R)
+@inline function plate_dipole_image(r::T,d::T,zplate) where T
+    zrel = r[3] - zplate
+    # position is reflected in z
+    rp = T( r[1],  r[2], r[3]-2zrel)
+    # dipole moment is reflected in x and y
+    dp = T(-d[1], -d[2], d[3])
+    return rp, dp
+end
+
+@inline function sphere_chargedipole_image(r,q,d,R)
     ar2 = sum(abs2,r)
     ar = sqrt(ar2)
     ar3 = ar2*ar
@@ -82,8 +91,8 @@ function sphere_chargedipole_images(r,q,d,R)
     qp = R*(rdotd/ar3 - q/ar)
     return rp, qp, dp
 end
-function sphere_chargedipole_images(r,q,d,R,R0)
-    rp, qp, dp = sphere_chargedipole_images(r-R0,q,d,R)
+@inline function sphere_chargedipole_image(r,q,d,R,R0)
+    rp, qp, dp = sphere_chargedipole_image(r-R0,q,d,R)
     return rp + R0, qp, dp
 end
 
@@ -95,7 +104,7 @@ function twosphere_chargedipole_images(r0,q0,d0,Rs,R0s,tol=1e-6)
         norm(r0-R0s[ii%2+1])<=Rs[ii%2+1] && continue
         rs, qs, ds = [r0], [q0], [d0]
         while norm(ds[end])>tol || abs(qs[end])>tol
-            push!.((rs, qs, ds), sphere_chargedipole_images(rs[end],qs[end],ds[end],Rs[ii%2+1],R0s[ii%2+1]))
+            push!.((rs, qs, ds), sphere_chargedipole_image(rs[end],qs[end],ds[end],Rs[ii%2+1],R0s[ii%2+1]))
             ii += 1
         end
         # take 2:end-1 because the first one is the original dipole and the last one is below the cutoff
@@ -107,7 +116,7 @@ function twosphere_chargedipole_images(r0,q0,d0,Rs,R0s,tol=1e-6)
 end
 
 # stateful iterator over image chargedipoles
-mutable struct Twosphere_Chargedipole_Images
+mutable struct Twosphere_Chargedipole_ImageIter
     Rs::Tuple{Float64,Float64}
     R0s::Tuple{SV3,SV3}
     r::SV3
@@ -115,57 +124,92 @@ mutable struct Twosphere_Chargedipole_Images
     d::SV3
     nextsphere::Int
 end
-
-function Base.iterate(iter::Twosphere_Chargedipole_Images,state=nothing)
-    iter.r, iter.q, iter.d = sphere_chargedipole_images(iter.r,iter.q,iter.d,iter.Rs[iter.nextsphere],iter.R0s[iter.nextsphere])
+function Base.iterate(iter::Twosphere_Chargedipole_ImageIter,state=nothing)
+    iter.r, iter.q, iter.d = sphere_chargedipole_image(iter.r,iter.q,iter.d,iter.Rs[iter.nextsphere],iter.R0s[iter.nextsphere])
     # 1 -> 2, 2 -> 1
     iter.nextsphere = 3 - iter.nextsphere
     return (iter.r, iter.q, iter.d), nothing
 end
 
-function dipole_image_interaction(Rs,R0s,r0,d0,tol=1e-7)
-    isfirst = false
-    iter = Twosphere_Chargedipole_Images(Rs,R0s,r0,0.,d0,1)
-    (r,q,d), state = iterate(iter)
-    V0 = V_chargedipole_dipole(r,q,d,r0,d0)
-    V = V0
-    ii = 0
-    ismall = 0
-    for (r,q,d) in iter
-        ii += 1
-        Vc = V_chargedipole_dipole(r,q,d,r0,d0)
-        V += Vc
-        ismall = abs(Vc)/abs(V) < tol ? ismall+1 : 0
-        ismall > 3 && break
-    end
-    V, ii
+# stateful iterator over image dipoles of two parallel plates at zs
+mutable struct Twoplate_Dipole_ImageIter
+    zs::Tuple{Float64,Float64}
+    r::SV3
+    d::SV3
+    nextplate::Int
+end
+function Base.iterate(iter::Twoplate_Dipole_ImageIter,state=nothing)
+    iter.r, iter.d = plate_dipole_image(iter.r,iter.d,iter.zs[iter.nextplate])
+    # 1 -> 2, 2 -> 1
+    iter.nextplate = 3 - iter.nextplate
+    return (iter.r, iter.d), nothing
 end
 
-function n_dipole_interactions(rs, ds, Rs, R0s, tol=1e-6)
-    myimgs(r,d) = twosphere_chargedipole_images(r,0.,d,Rs,R0s,tol)
-    cdims = myimgs.(rs,ds)
-    # length(ri)-1 because the first element is the original dipole
-    imgsizes = [length(cdi.rs)-1 for cdi in cdims]
-    imstarts = cumsum([1;imgsizes])
-    # all images in a row (imstarts[i]:imstarts[i+1] should give images of dipole i)
-    rims = Vector{SV3}(undef,imstarts[end]-1)
-    qims = similar(rims,Float64)
-    dims = similar(rims)
-    for ii=1:length(cdims)
-        # cdims 2:end only (without original dipole)
-        rims[imstarts[ii]:imstarts[ii+1]-1] = cdims[ii].rs[2:end]
-        qims[imstarts[ii]:imstarts[ii+1]-1] = cdims[ii].qs[2:end]
-        dims[imstarts[ii]:imstarts[ii+1]-1] = cdims[ii].ds[2:end]
+function twoplate_V_dipole_dipole(zs,r1,d1,r2,d2,tol=1e-6)
+    # interaction of dipole d1 at r1 with dipole r2 d2 and its images
+    Vdd = V_dipole_dipole(r2,d2,r1,d1)
+    Vdi = 0.
+    Vmax = r1==r2 ? 0. : abs(Vdd)
+    for iplate = 1:2
+        ii = 0
+        ismall = 0
+        for (r,d) in Twoplate_Dipole_ImageIter(zs,r2,d2,iplate)
+            ii += 1
+            Vc = V_dipole_dipole(r,d,r1,d1)
+            Vdi += Vc
+            Vmax = max(Vmax,abs(Vc))
+            ismall = abs(Vc)/abs(Vmax) < tol ? ismall+1 : 0
+            ismall > 2 && break
+        end
+        #println("did isphere = $isphere, ii = $ii")
     end
-    cdim = ChargesDipoles(rims,qims,dims)
-    pprintln("length(rims) = $(length(rims))")
-    Vdd, Vid = V_dipoles_images(rs,ds,cdim.rs,cdim.qs,cdim.ds)
-    Vid_r = similar(Vdd)
+    Vdd, Vdi
+end
+
+function twosphere_V_dipole_dipole(Rs,R0s,r1,d1,r2,d2,tol=1e-6)
+    # interaction of dipole d1 at r1 with dipole r2 d2 and its images
+    Vdd = V_dipole_dipole(r2,d2,r1,d1)
+    Vdi = 0.
+    Vmax = r1==r2 ? 0. : abs(Vdd)
+    for isphere = 1:2
+        ii = 0
+        ismall = 0
+        for (r,q,d) in Twosphere_Chargedipole_ImageIter(Rs,R0s,r2,0.,d2,isphere)
+            ii += 1
+            Vc = V_chargedipole_dipole(r,q,d,r1,d1)
+            Vdi += Vc
+            Vmax = max(Vmax,abs(Vc))
+            ismall = abs(Vc)/abs(Vmax) < tol ? ismall+1 : 0
+            ismall > 2 && break
+        end
+        #println("did isphere = $isphere, ii = $ii")
+    end
+    Vdd, Vdi
+end
+
+function environ_n_dipole_interactions(Vdidfun, rs, ds)
+    N = length(rs)
+    N == length(ds) || error("length(rs) = $(length(rs)) must be equal to length(ds) = $(length(ds))!")
+    Vdd = Array{Float64}(undef,N,N)
+    Vid = similar(Vdd)
     for ii = 1:length(rs)
-        Vid_r[ii,:] .= dropdims(sum(Vid[imstarts[ii]:imstarts[ii+1]-1,:],dims=1),dims=1)
+        for jj = 1:ii
+            Vdd[ii,jj], Vid[ii,jj] = Vdidfun(rs[ii],ds[ii],rs[jj],ds[jj])
+            Vdd[jj,ii], Vid[jj,ii] = Vdd[ii,jj], Vid[ii,jj]
+        end
+        Vdd[ii,ii] = 0.
     end
-    cd = ChargesDipoles(rs,zeros(length(rs)),ds)
-    return (Rs=Rs, R0s=R0s, cd=cd, cdim=cdim, Vdd=Vdd, Vid=Vid, Vid_r=Vid_r, imstarts=imstarts)
+    return Vdd,Vid
+end
+
+function twoplate_n_dipole_interactions(zs, rs, ds, tol=1e-6)
+    Vdidfun(r1,d1,r2,d2) = twoplate_V_dipole_dipole(zs,r1,d1,r2,d2,tol)
+    environ_n_dipole_interactions(Vdidfun, rs, ds)
+end
+
+function twosphere_n_dipole_interactions(Rs, R0s, rs, ds, tol=1e-6)
+    Vdidfun(r1,d1,r2,d2) = twosphere_V_dipole_dipole(Rs,R0s,r1,d1,r2,d2,tol)
+    environ_n_dipole_interactions(Vdidfun, rs, ds)
 end
 
 end # module
